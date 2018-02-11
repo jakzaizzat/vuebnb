17d5ce8919aa61681225b3559f100167d307916e
Aizzat <jakzaizzat@gmail.com>
2018-02-11 18:09:58 +0800

    Full intergrate laravel aeland vuejs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Listing;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +14,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+
+Route::get('/listing/{listing}', 'ListingController@get_listing_web');
